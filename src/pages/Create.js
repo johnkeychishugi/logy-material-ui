71d87443f013a7b0348e4eacf8b8222a69e85e67
John Chishugi <jkchishugi@gmail.com>
2021-10-20 00:01:03 +0100

    ft-radio-button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { Typography, Button, Container } from '@mui/material';
+import { useState } from 'react'
+import { Typography, Button, Container, TextField, Radio, 
+         RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
 import {  makeStyles } from '@mui/styles';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const useStyles = makeStyles({
-
+  field:{
+    marginTop: '20px !important',
+    marginBottom: '20px !important',  
+    display: 'block'
+  }
 });
 
 const Create = () => {
+  const [title,setTitle] = useState('');
+  const [details,setDetails] = useState('');
+  const [titleError,setTitleError] = useState(false);
+  const [detailsError,setDetailsError] = useState(false);
+  const [category,setCategory] = useState('todos');
   const classes = useStyles();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+
+    if(title === ''){
+       setTitleError(true);
+    }
+
+    if(details === ''){
+      setDetailsError(true);
+   }
+
+    if(title && details){
+      console.log(title,details,category);
+    }
+  }
 
   return (
     <Container>
@@ -20,8 +48,42 @@ const Create = () => {
         >
           Create a New Note
         </Typography>
-        <Button
-          onClick={ ()=>{console.log('You clicked me') }}
+        <form noValidate autoComplete="off"  onSubmit={handleSubmit}>
+          <TextField 
+            onChange={(e) => setTitle(e.target.value)}
+            className={classes.field}
+            label="Note Title"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+            helperText={titleError ? 'Title is required' : ''}
+          />
+          <TextField 
+            onChange={(e) => setDetails(e.target.value)}
+            className={classes.field}
+            label="Details"
+            variant="outlined"
+            color="secondary"
+            multiline
+            rows={4}
+            fullWidth
+            required
+            error={detailsError}
+            helperText={detailsError ? 'Details is required' : ''}
+          />
+          <FormControl className={classes.field}>
+            <FormLabel>Note Category</FormLabel>
+            <RadioGroup value={category} onChange={(e)=> setCategory(e.target.value) }>
+                <FormControlLabel value="money" control={<Radio color="secondary" />} label="Money"/>
+                <FormControlLabel value="todos" control={<Radio color="secondary" />} label="Todos"/>
+                <FormControlLabel value="reminders" control={<Radio color="secondary" />} label="Reminders"/>
+                <FormControlLabel value="work" control={<Radio color="secondary" />} label="Work"/>
+            </RadioGroup>
+          </FormControl>
+          <br />
+           <Button
           type="submit"
           color="secondary"
           variant="contained"
@@ -29,6 +91,7 @@ const Create = () => {
         >
           Submit
         </Button>
+        </form>
     </Container>
   )
 }
